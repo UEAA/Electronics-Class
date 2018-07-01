667f24f5261e1fbd9db9fb12ccea8f51b55c9e52
Alan Chen <alanchen@nyu.edu>
2018-07-01 00:43:22 -0400

    Electronics Lesson 3 2018

---
+++ Class 3/2018/traffic_light/traffic_light.ino
@@ -0,0 +1,40 @@
+int GREEN = 8;
+int YELLOW = 9;
+int RED = 10;
+
+void setup() {
+   pinMode(GREEN, OUTPUT);
+   pinMode(YELLOW, OUTPUT);
+   pinMode(RED, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(YELLOW, LOW);   
+  digitalWrite(RED, LOW);   
+  delay(5000);
+
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, HIGH);   
+  digitalWrite(RED, LOW); 
+  delay(200);
+  
+  digitalWrite(YELLOW, LOW);   
+  delay(200);
+
+  digitalWrite(YELLOW, HIGH);   
+  delay(200);
+
+  digitalWrite(YELLOW, LOW);   
+  delay(200);
+
+  digitalWrite(YELLOW, HIGH);   
+  delay(200);
+
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, LOW);   
+  digitalWrite(RED, HIGH); 
+  delay(3000);                 
+}
+
